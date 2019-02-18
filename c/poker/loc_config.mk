b0d537c1b75eb5078c91a32b487f7434645344f0
lichou <lichou@lichou.com>
2019-02-18 22:37:23 +0800

    add poker project

---
+++ c/poker/loc_config.mk
@@ -0,0 +1,3 @@
+BIN = 
+SHARE = ../$(LIB_PATH_NAME)/libpoker.so
+ARCHIVE = ../$(LIB_PATH_NAME)/libpoker.a
---
+++ c/poker/poker.c
@@ -0,0 +1,7 @@
+#include "defs.h"
+
+
+const short red_joker_ = MAX_Number + 2;
+const short black_joker_ = MAX_Number + 1;
+const char max_cards_in_deck_ = MAX_Number * MAX_Suit + 2;
+const char max_decks_ = 8;
